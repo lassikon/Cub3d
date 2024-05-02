8cfc29a9f979831c0c3e0a8ff8ca3eeae43e79c8
Janrau Beray <jberay@c1r6p18.hive.fi>
2024-05-02 15:52:35 +0300

    parsing

--- map_parsing.c
+++ map_parsing.c
@@ -6,11 +6,51 @@
 /*   By: jberay <jberay@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 11:26:02 by jberay            #+#    #+#             */
-/*   Updated: 2024/05/02 15:33:27 by jberay           ###   ########.fr       */
+/*   Updated: 2024/05/02 15:51:47 by jberay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	parse_color(int *color, char *line)
+{
+	int		i;
+	int		j;
+	char	**split;
+
+	i = 0;
+	j = 0;
+	split = ft_split(line, ',');
+	while (split[i])
+	{
+		color[j] = ft_atoi(split[i]);
+		i++;
+		j++;
+	}
+}
+
+int	check_texture(t_scene *scene)
+{
+	if (scene->no_texture == NULL)
+		return (0);
+	if (scene->so_texture == NULL)
+		return (0);
+	if (scene->we_texture == NULL)
+		return (0);
+	if (scene->ea_texture == NULL)
+		return (0);
+	if (scene->floor_color[0] == -1 || scene->floor_color[1] == -1 || scene->floor_color[2] == -1)
+		return (0);
+	return (1);
+}
+
+void	parse_map(t_scene *scene, t_list **head)
+(
+	int	map_size;
+
+	map_size = ft_lstsize(*head);
+	
+)
 
 static void	call_gnl(t_scene *scene, int map_fd)
 {
@@ -22,8 +62,8 @@ static void	call_gnl(t_scene *scene, int map_fd)
 	scene->so_texture = NULL;
 	scene->we_texture = NULL;
 	scene->ea_texture = NULL;
-	ft_bzero(scene->floor_color, 3);
-	ft_bzero(scene->ceiling_color, 3);
+	ft_memset(scene->floor_color, -1, 3);
+	ft_memset(scene->ceiling_color, -1, 3);
 	scene->map = NULL;
 	while (1)
 	{
@@ -36,13 +76,28 @@ static void	call_gnl(t_scene *scene, int map_fd)
 	printf("list size: %d\n", ft_lstsize(head));
 	while(head)
 	{
-		printf("type: %d\n", ((t_token *)head->content)->type);
-		printf("line: %s\n", ((t_token *)head->content)->line);
-		size_t len = ((t_token *)head->content)->location.len - 1;
-		size_t start = ((t_token *)head->content)->location.start;
-		printf("start: %zu\n", start);
-		printf("len: %zu\n", len);
-		printf("value: %s\n",ft_substr((char *)((t_token *)head->content)->line, start, len));
+		char *t_line = ((t_token *)head->content)->line;
+		t_type t_type = ((t_token *)head->content)->type;
+		size_t t_len = ((t_token *)head->content)->location.len - 1;
+		size_t t_start = ((t_token *)head->content)->location.start;
+		printf("start: %zu\n", t_start);
+		printf("len: %zu\n", t_len);
+		printf("value: %s\n",ft_substr(t_line, t_start, t_len));
+
+		if (t_type == NO)
+			scene->no_texture = ft_substr(t_line, t_start, t_len);
+		if (t_type == SO)
+			scene->so_texture = ft_substr(t_line, t_start, t_len);
+		if (t_type == WE)
+			scene->we_texture = ft_substr(t_line, t_start, t_len);
+		if (t_type == EA)
+			scene->ea_texture = ft_substr(t_line, t_start, t_len);
+		if (t_type == F)
+			parse_color(scene->floor_color, ft_substr(t_line, t_start, t_len));
+		if (t_type == C)
+			parse_color(scene->ceiling_color, ft_substr(t_line, t_start, t_len));
+		if (t_type == MAP && check_texture(scene))
+			parse_map(scene, &head);
 		head = head->next;
 	}
 }
