3d7135458933d686771dfc30ae939cd72482d688
janrau <janraup356@gmail.com>
2024-05-11 10:47:47 +0300

    fixed floor parenthesis

--- render_walls.c
+++ render_walls.c
@@ -6,7 +6,7 @@
 /*   By: janraub <janraub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 18:46:13 by lkonttin          #+#    #+#             */
-/*   Updated: 2024/05/11 07:46:43 by janraub          ###   ########.fr       */
+/*   Updated: 2024/05/11 10:46:24 by janraub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ static void	draw_column(t_game *game, t_ray *ray, mlx_image_t *img)
 		game->render.top_wall = 0;
 	}
 	ray->tx = (float)(img->width / TILE_SIZE) * ray->col;
-	ray->tx = ((int)ray->tx % img->width);
-	ray->ty = ((int)ray->ty % img->height);
+/* 	ray->tx = ((int)ray->tx % img->width);
+	ray->ty = ((int)ray->ty % img->height); */
 	while (y < ray->height)
 	{
 		ray->pixel = &img->pixels[(int)ray->ty * img->width * 4 + (int)ray->tx * 4];
@@ -62,28 +62,32 @@ static void	draw_column(t_game *game, t_ray *ray, mlx_image_t *img)
 	y = (int)game->render.bottom_wall;
 	while (y < SCREEN_HEIGHT)
 	{
-		float ratio = game->p.height / y - game->vertical_center;
-		ray->tx = game->p.x  + cos(ray->angle)*ratio*game->dist_to_proj_plane/fishbowl(ray->angle, game->p.angle);
-		ray->ty = game->p.y + sin(ray->angle)*ratio*game->dist_to_proj_plane/fishbowl(ray->angle, game->p.angle);
-		//ray->tx = fmod(ray->tx, TILE_SIZE); // Wrap texture coordinates around the tile size
-		//ray->ty = fmod(ray->ty, TILE_SIZE);
-		if (ray->tx < 0)
-			ray->tx += TILE_SIZE; // Ensure positive texture coordinates
-		if (ray->ty < 0)
-			ray->ty += TILE_SIZE;
-		ray->tx = ray->tx - (int)(ray->tx / TILE_SIZE) * TILE_SIZE;
-		ray->ty = ray->ty - (int)(ray->ty / TILE_SIZE) * TILE_SIZE;
-		ray->tx = (float)img->width / TILE_SIZE * ray->tx;
-		ray->ty = (float)img->height / TILE_SIZE * ray->ty;
-		ray->tx = ((int)ray->tx % img->width);
-		ray->ty = ((int)ray->ty % img->height);
-		ray->pixel = &img->pixels[(int)ray->ty * img->width * 4 + (int)ray->tx * 4];
-		if (ray->pixel[3] != 0)
+		float fy = y - game->vertical_center;
+		if (fy != 0)
 		{
-			color = get_rgba(ray->pixel[0], ray->pixel[1], ray->pixel[2], ray->pixel[3]);
-			mlx_put_pixel(game->image, ray->column, y, color);
+			float ratio = game->p.height / fy;
+			ray->tx = game->p.x  + cos(ray->angle)*ratio*game->dist_to_proj_plane/fishbowl(ray->angle, game->p.angle);
+			ray->ty = game->p.y + sin(ray->angle)*ratio*game->dist_to_proj_plane/fishbowl(ray->angle, game->p.angle);
+			//ray->tx = fmod(ray->tx, TILE_SIZE); // Wrap texture coordinates around the tile size
+			//ray->ty = fmod(ray->ty, TILE_SIZE);
+			if (ray->tx < 0)
+				ray->tx += TILE_SIZE; // Ensure positive texture coordinates
+			if (ray->ty < 0)
+				ray->ty += TILE_SIZE;
+			ray->tx = ray->tx - (int)(ray->tx / TILE_SIZE) * TILE_SIZE;
+			ray->ty = ray->ty - (int)(ray->ty / TILE_SIZE) * TILE_SIZE;
+			ray->tx = (float)img->width / TILE_SIZE * ray->tx;
+			ray->ty = (float)img->height / TILE_SIZE * ray->ty;
+			ray->tx = ((int)ray->tx % img->width);
+			ray->ty = ((int)ray->ty % img->height);
+			ray->pixel = &img->pixels[(int)ray->ty * img->width * 4 + (int)ray->tx * 4];
+			if (ray->pixel[3] != 0)
+			{
+				color = get_rgba(ray->pixel[0], ray->pixel[1], ray->pixel[2], ray->pixel[3]);
+				mlx_put_pixel(game->image, ray->column, y, color);
+			}
+			y++;
 		}
-		y++;
 	}
 	/*draw ceiling*/
 	y = (int)game->render.top_wall;
